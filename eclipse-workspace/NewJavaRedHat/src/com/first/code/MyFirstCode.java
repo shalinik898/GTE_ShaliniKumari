7a3e9a8598e9cfc6c8e30ddee604e7f2035640ea
Shalini Kumari <shalinik898@gmail.com>
2021-06-10 09:54:36 +0530

    Hello world

---
+++ eclipse-workspace/NewJavaRedHat/src/com/first/code/MyFirstCode.java
@@ -0,0 +1,23 @@
+package com.first.code;
+
+public class MyFirstCode {
+	private int a;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public static void main(String[] args) {
+		extracted();
+	}
+
+	private static void extracted() {
+		String text = "Shalini";
+		System.out.println(text);
+	}
+
+}
